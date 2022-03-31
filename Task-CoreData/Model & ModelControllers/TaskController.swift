d218f4f2111596f7f08808bb4013f0b3c7038984
James Hager <james@avantiapplications.com>
2022-03-31 08:25:20 -0600

    7 - Simplify TaskController.

--- Task-CoreData/Model & ModelControllers/TaskController.swift
+++ Task-CoreData/Model & ModelControllers/TaskController.swift
@@ -14,19 +14,14 @@ class TaskController {
     var tasks = [Task]()
     
     private var project: Project!
-    private var allTasks = [Task]()
     
-    private init() {
-        showUncompleted()
-    }
+    private init() { }
     
     // MARK: - Assign Tasks
     
     func setProject(_ project: Project) {
         self.project = project
-        allTasks = Array(project.tasks as! Set<Task>)
         showUncompleted()
-        print("\(#function) - name: \(project.name), allTasks.count: \(allTasks.count), tasks.count: \(tasks.count)")
     }
     
     // MARK: - CRUD
@@ -37,11 +32,13 @@ class TaskController {
     }
     
     func showUncompleted() {
-        tasks = allTasks.filter { !$0.isComplete}
+        guard let project = project else { return }
+        tasks = Array(project.tasks as! Set<Task>).filter { !$0.isComplete}
     }
     
     func showCompleted() {
-        tasks = allTasks.filter { $0.isComplete}
+        guard let project = project else { return }
+        tasks = Array(project.tasks as! Set<Task>).filter { $0.isComplete}
     }
     
     func update(_ task: Task, name: String, notes: String?, dueDate: Date?) {
